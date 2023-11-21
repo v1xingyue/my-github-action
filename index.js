4820fc81f764a66be2d31236b9f777507d587c1e
v1xingyue <qixingyue@gmail.com>
2023-11-21 18:21:02 +0800

    test sui get coins

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import { getInput, setOutput, setFailed } from "@actions/core";
 import { context } from "@actions/github";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui.js/client";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  setFailed(error.message);
-}
+const myfun = async () => {
+  try {
+    const client = new SuiClient({ url: getFullnodeUrl("devnet") });
+    const coins = await client.getCoins({
+      owner:
+        "0xc4d17bdea567268b50cb24c783ccafc678d468a0cfce0afb84313b163cb403ef",
+    });
+    console.log(coins);
+
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    setFailed(error.message);
+  }
+};
+
+myfun();
